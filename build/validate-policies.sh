6775b096d003374fabbd38aa1ec2625529da4c58
Brian Jarvis <bjarvis@redhat.com>
2025-07-08 10:06:21 -0400

    enable process to skip validation for kustomize path

--- build/validate-policies.sh
+++ build/validate-policies.sh
@@ -9,27 +9,33 @@ export POLICY_GEN_ENABLE_HELM=true
 
 # Validate the policy resource file under the directory provided
 validatePolicies() {
-	find $1 -name "*.yaml" | xargs kubeconform -schema-location 'schemas/{{ .ResourceKind }}_{{ .ResourceAPIVersion }}.json' -summary
+  find $1 -name "*.yaml" | xargs kubeconform -schema-location 'schemas/{{ .ResourceKind }}_{{ .ResourceAPIVersion }}.json' -summary
 }
 
 # Validate the generator based policies under the directory provided
 validatePolicyGenerator() {
-	KPATH=`pwd`
-	SETPATH=$1
-	cd $SETPATH
-	for project in `find . -name "kustomization.y*ml" | sed 's,/kustomization.*,,g'`; do
-	        cd $project
-		echo "Generating Policys $project"
+  KPATH=`pwd`
+  SETPATH=$1
+  cd $SETPATH
+  for kst in `find . -name "kustomization.y*ml"`; do
+    project=`echo $kst | sed 's,/kustomization.*,,g'`
+
+    # skip any kustomize files that are labeled with skip_validation: true
+    if grep -q "skip_validation: true" $kst; then
+      echo "Skipping validation for $kst."
+    fi
+    cd $project
+    echo "Generating Policys $project"
           KUSTOMIZE_OUTPUT=${KUSTOMIZE_OUTPUT_ROOT}/${project}/kbout.yml
           mkdir -p "$(dirname ${KUSTOMIZE_OUTPUT})"
-	        OUTPUT=`kustomize build --enable-alpha-plugins --enable-helm > ${KUSTOMIZE_OUTPUT}`
+          OUTPUT=`kustomize build --enable-alpha-plugins --enable-helm > ${KUSTOMIZE_OUTPUT}`
 
           cd $KPATH
           cat ${KUSTOMIZE_OUTPUT} |  kubeconform -schema-location 'schemas/{{ .ResourceKind }}_{{ .ResourceAPIVersion }}.json' -schema-location default -summary
 
-	        cd $KPATH/$SETPATH
-	done
-	cd $KPATH
+          cd $KPATH/$SETPATH
+  done
+  cd $KPATH
 }
 
 # Setup 
@@ -41,8 +47,8 @@ mkdir -p ${GOBIN}
 
 # Configure org for manual validation runs
 if [ -z "${OCM_REPOSITORY_OWNER}" ]; then
-	echo "Set OCM_REPOSITORY_OWNER=[github org], using stolostron by default"
-	OCM_REPOSITORY_OWNER=stolostron
+  echo "Set OCM_REPOSITORY_OWNER=[github org], using stolostron by default"
+  OCM_REPOSITORY_OWNER=stolostron
 fi
 
 set -euo pipefail  # exit on errors and unset vars, and stop on the first error in a "pipeline"
@@ -53,24 +59,24 @@ go install github.com/yannh/kubeconform/cmd/kubeconform@$KC_VERSION
 
 # Get the CRDs needed for policy validation
 if [ ! -d schemas ]; then
-	echo "Getting Schema conversion tool"
-	mkdir schemas
-	cd schemas
-	curl -s -o crd-schema.py https://raw.githubusercontent.com/yannh/$KUBECONFORM/$KC_VERSION/scripts/openapi2jsonschema.py
-	chmod a+x crd-schema.py
+  echo "Getting Schema conversion tool"
+  mkdir schemas
+  cd schemas
+  curl -s -o crd-schema.py https://raw.githubusercontent.com/yannh/$KUBECONFORM/$KC_VERSION/scripts/openapi2jsonschema.py
+  chmod a+x crd-schema.py
 
-	echo "Converting CRDs to Schemas"
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_placementbindings.yaml
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_policies.yaml
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_policysets.yaml
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_policyautomations.yaml
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_02_clusters.open-cluster-management.io_placements.crd.yaml
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_00_clusters.open-cluster-management.io_managedclusters.crd.yaml
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_00_clusters.open-cluster-management.io_managedclustersets.crd.yaml
-	./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_01_clusters.open-cluster-management.io_managedclustersetbindings.crd.yaml
+  echo "Converting CRDs to Schemas"
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_placementbindings.yaml
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_policies.yaml
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_policysets.yaml
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/governance-policy-propagator/main/deploy/crds/policy.open-cluster-management.io_policyautomations.yaml
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_02_clusters.open-cluster-management.io_placements.crd.yaml
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_00_clusters.open-cluster-management.io_managedclusters.crd.yaml
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_00_clusters.open-cluster-management.io_managedclustersets.crd.yaml
+  ./crd-schema.py https://raw.githubusercontent.com/${OCM_REPOSITORY_OWNER}/placement/main/deploy/hub/0000_01_clusters.open-cluster-management.io_managedclustersetbindings.crd.yaml
 
 
-	cd ..
+  cd ..
 fi
 
 
